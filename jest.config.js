6203fd102740f14d16403b8a91ad5cb39bd8b11e
Gregor Adams <greg@pixelass.com>
2022-08-03 08:18:27 +0200

    chore: add test workflow

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const { defaults } = require('jest-config');
+const {defaults} = require('jest-config');
 module.exports = {
 	...defaults,
 	resolver: `${__dirname}/jest-resolver.js`,
--- src/components/Button/Button.spec.js
+++ src/components/Button/Button.spec.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
